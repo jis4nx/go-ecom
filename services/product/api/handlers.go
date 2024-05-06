5ad7e6aba3a0c22f665840e2f634290d6c8b01ea
Rakibul Islam <jis4nx@gmail.com>
2024-05-06 22:07:06 +0600

    :sparkels: feat: Implement Create Product Handler

--- services/product/api/handlers.go
+++ services/product/api/handlers.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/jis4nx/go-ecom/helpers"
 	"github.com/jis4nx/go-ecom/services/product/internals/productmodel"
 	"github.com/jis4nx/go-ecom/services/product/internals/rabbit/publisher"
+	"github.com/jis4nx/go-ecom/services/product/types"
 	"go.uber.org/zap"
 )
 
@@ -20,11 +22,17 @@ func (p *ProductApp) SetProductApp(a *helpers.App) {
 
 func (a *ProductApp) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
-	input := productmodel.Product{}
+	input := types.ProductInput{}
 
 	if err := a.ReadJson(w, r, &input); err != nil {
 		a.Logger.Warn("Failed to Read json", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
-	publisher.DispatchProductEvent(ctx, a.Rabbit, publisher.ProductCreated, input)
+
+	p := productmodel.NewProduct(input)
+	fmt.Println(p)
+
+
+  // Publishing the product to rabbitmq queue
+	publisher.DispatchProductEvent(ctx, a.Rabbit, publisher.ProductCreated, p)
 }
